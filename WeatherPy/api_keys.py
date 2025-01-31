6de87e2fdac8a28471ce9b0175942c0e4f9ce247
jennsv97 <vegajennifer91@gmail.com>
2025-01-30 17:58:50 -0800

    add png images, updated script, and maps for weatherpy and vactionpy

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "a5ad5ee29b3f7e505976f7eeb0f07614"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5eb1ae3ef85e4673b82a470d60849f11"
